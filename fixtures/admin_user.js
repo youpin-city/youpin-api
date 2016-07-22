6b2e90ad59e7b26312529bfc1401a2c7974d0453
parnurzeal <parnurzeal@gmail.com>
2016-07-23 02:20:53 +0700

    Add test to /users/:id route

--- fixtures/admin_user.js
+++ fixtures/admin_user.js
@@ -1,6 +1,6 @@
 const ObjectId = require('mongoose').Types.ObjectId;
 
-exports.User = [{
+module.exports = {
   _id: new ObjectId,
   name: 'YouPin Admin',
   phone: '081-985-2586',
@@ -9,4 +9,4 @@ exports.User = [{
   password: '$2a$10$iorOMFOPboPeF20W20DKruey2UXXa4eOQSuReOMlxXnqNe5t6Egaq',
   email: 'contact@youpin.city',
   role: 'admin',
-}];
+};
