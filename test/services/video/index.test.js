f11b15dbd5465aeba5b3650e17c81f242f01c012
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-03 00:16:56 +0700

    Fix lint for video index test

--- test/services/video/index.test.js
+++ test/services/video/index.test.js
@@ -3,33 +3,32 @@ const assertTestEnv = require('../../test_helper').assertTestEnv;
 const expect = require('../../test_helper').expect;
 
 // Models
-const VideoModel = require('../../../src/services/video/video-model.js');;
+const VideoModel = require('../../../src/services/video/video-model.js');
 
 // App stuff
 const app = require('../../../src/app');
-const mongoose = require('mongoose')
 
 // Exit test if NODE_ENV is not equal `test`
 assertTestEnv();
 
-describe('video service', function() {
+describe('video service', () => {
   let server;
 
   before((done) => {
     server = app.listen(9100);
     server.once('listening', () => {
-      done()
+      done();
     });
   });
 
   after((done) => {
     // Clears collection after finishing all tests.
     VideoModel.remove({})
-    .then((_) => {
+    .then(() => {
       server.close((err) => {
         if (err) return done(err);
 
-        done();
+        return done();
       });
     });
   });
