7fcdae4dcc583eb5eb97fd52f06c78b21d66bcd9
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-03 00:33:40 +0700

    Fix lint for index and app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
-server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
-);
+server.on('listening', () => {
+  console.log(`Feathers application started on ${app.get('host')}:${port}`);
+});
