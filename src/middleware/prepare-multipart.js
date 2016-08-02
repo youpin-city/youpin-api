a030c586530b81185f2cceca2ca8216f1166b5a3
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-03 01:50:45 +0700

    Fix lint for middlewares

--- src/middleware/prepare-multipart.js
+++ src/middleware/prepare-multipart.js
@@ -1,17 +1,13 @@
-'use strict';
-
-
 // Middleware to handle file uploading
-module.exports = function(fieldName, fileSize) {
+module.exports = function (fieldName, fileSize) { // eslint-disable-line func-names
+  const uploader = require('../utils/uploader')(fileSize); // eslint-disable-line global-require
 
-  const uploader = require('../utils/uploader')(fileSize);
-
-  return function(req, res, next) {
+  return (req, res, next) => {
     // Bypass this middleware if it's not a POST request
     if (req.method.toLowerCase() === 'post') {
       return uploader.single(fieldName)(req, res, next);
     }
 
-    next();
-  }
+    return next();
+  };
 };
