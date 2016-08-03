d97d21e4c316afb7610e64b47a1619122a25146a
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-03 21:30:58 +0700

    Refactor by creating a GCS Uploader class to be used in photo and video service

--- src/services/video/index.js
+++ src/services/video/index.js
@@ -1,59 +1,13 @@
 const errors = require('feathers-errors');
-const gcloud = require('gcloud');
 const Promise = require('bluebird');
 const hooks = require('./hooks');
 const Video = require('./video-model');
+const GCSUploader = require('../../utils/gcs-uploader');
 
 // Middleware for handling file upload
 const VIDEO_SIZE = 20 * 1024 * 1024;
 const prepareMultipart = require('../../middleware/prepare-multipart')('video', VIDEO_SIZE);
 const attachFileToFeathers = require('../../middleware/attach-file-to-feathers')();
-
-function getGCSBucketFile(gcsFileName, gcsConfig) {
-  const gcs = gcloud.storage({
-    projectId: gcsConfig.projectId,
-    keyFilename: gcsConfig.keyFile,
-  });
-  const bucket = gcs.bucket(gcsConfig.bucket);
-  const bucketFile = bucket.file(gcsFileName);
-
-  return bucketFile;
-}
-
-function getGCSPublicUrl(gcsFileName, gcsConfig) {
-  return `${gcsConfig.gcsUrl}/${gcsConfig.bucket}/${gcsFileName}`;
-}
-
-function uploadToGCS(reqFile, gcsConfig) {
-  return new Promise((resolve, reject) => { // eslint-disable-line consistent-return
-    if (!reqFile) {
-      return reject(new Error('No file provided'));
-    }
-
-    const gcsFileName = `${Date.now()}_${reqFile.originalname}`;
-    const bucketFile = getGCSBucketFile(gcsFileName, gcsConfig);
-    const stream = bucketFile.createWriteStream();
-
-    stream.on('error', (err) => {
-      reqFile.cloudStorageError = err; // eslint-disable-line no-param-reassign
-
-      return reject(err);
-    });
-
-    stream.on('finish', () => {
-      const publicUrl = getGCSPublicUrl(gcsFileName, gcsConfig);
-
-      /* eslint-disable no-param-reassign */
-      reqFile.cloudStorageObject = gcsFileName;
-      reqFile.cloudStoragePublicUrl = publicUrl;
-      /* eslint-enable no-param-reassign */
-
-      return resolve(reqFile);
-    });
-
-    stream.end(reqFile.buffer);
-  });
-}
 
 // Save video metadata to database
 function saveVideoMetadata(file) {
@@ -99,7 +53,6 @@ function respondWithVideoMetadata(videoDocument) {
   });
 }
 
-
 class VideosService {
   setup(app) {
     this.app = app;
@@ -117,8 +70,9 @@ class VideosService {
 
   create(data, params) {
     const gcsConfig = this.app.get('gcs');
+    const gcsUploader = new GCSUploader(gcsConfig);
 
-    return uploadToGCS(params.file, gcsConfig)
+    return gcsUploader.upload(params.file)
     .then((file) => saveVideoMetadata(file))
     .then((videoDoc) => respondWithVideoMetadata(videoDoc))
     .catch((err) => Promise.reject(err));
