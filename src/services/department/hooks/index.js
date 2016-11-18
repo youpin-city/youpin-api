e8008e2497f88740b981c297d9e8de533b487dda
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-19 02:05:40 +0700

    Allow only organization admin to create/update/path/remove department

--- src/services/department/hooks/index.js
+++ src/services/department/hooks/index.js
@@ -1,13 +1,44 @@
 const auth = require('feathers-authentication').hooks;
+const validateObjectId = require('../../../utils/hooks/validate-object-id-hook');
+const SUPER_ADMIN = require('../../../constants/roles').SUPER_ADMIN;
+const ORGANIZATION_ADMIN = require('../../../constants/roles').ORGANIZATION_ADMIN;
 
 exports.before = {
   all: [],
   find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: [],
+  get: [validateObjectId()],
+  create: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN, ORGANIZATION_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  update: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN, ORGANIZATION_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  patch: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN, ORGANIZATION_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  remove: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN, ORGANIZATION_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
 };
 
 exports.after = {
