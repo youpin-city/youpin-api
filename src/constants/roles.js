316f8fa325d16ae9a5fa804632bb056d83e5d7a5
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-19 01:40:45 +0700

    Only SUPER_ADMIN role can create/update/patch/delete organizations

---
+++ src/constants/roles.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SUPER_ADMIN: 'super_admin',
+  ORGANIZATION_ADMIN: 'organization_admin',
+  DEPARTMENT_HEAD: 'department_head',
+  DEPARTMENT_OFFICER: 'department_officer',
+  USER: 'user',
+};
--- src/services/organization/hooks/index.js
+++ src/services/organization/hooks/index.js
@@ -1,12 +1,45 @@
-// TODO: Add authentication hooks to each routing.
+const auth = require('feathers-authentication').hooks;
+const validateObjectId = require('../../../utils/hooks/validate-object-id-hook');
+const SUPER_ADMIN = require('../../../constants/roles').SUPER_ADMIN;
+
 exports.before = {
   all: [],
   find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: [],
+  get: [
+    validateObjectId(),
+  ],
+  create: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  update: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  patch: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
+  remove: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToRoles({
+      roles: [SUPER_ADMIN],
+      fieldName: 'role',
+    }),
+  ],
 };
 
 exports.after = {
