86392b0e7a48aa21b4c1c5e51d5190c2bccd87a8
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-21 17:12:40 +0700

    Disable console log when testing and write log to a file when not testing

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 const winston = require('winston');
 
 module.exports = function (app) { // eslint-disable-line func-names
   // Add a logger to our app object for convenience
   app.logger = winston; // eslint-disable-line no-param-reassign
+
+  // Get log setting from config file
+  const logConfig = app.get('logger');
+
+  // Let winston write logs to a file
+  if (process.env.NODE_ENV !== 'test') {
+    // Create a log folder if it does not exist
+    if (!fs.existsSync(logConfig.path)) {
+      fs.mkdirSync(logConfig.path);
+    }
+    // Set file to be written
+    winston.add(winston.transports.File, {
+      filename: path.join(logConfig.path, logConfig.filename),
+    });
+  } else {
+    // In test environment, do not print log to console
+    winston.remove(winston.transports.Console);
+  }
 
   return (error, req, res, next) => {
     if (error) {
