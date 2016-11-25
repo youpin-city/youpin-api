5510f725bb1941b90934af2ca488c3232a9e89a4
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-25 14:39:31 +0700

    Add test for prepare-activity-log hook

--- test/fixtures/pins.js
+++ test/fixtures/pins.js
@@ -4,29 +4,36 @@ module.exports = [
   {
     _id: '579334c75563625d6281b6f6',
     detail: casual.text,
-    organization: '57933111556362511181aaa1',
+    organization: '57933111556362511181aaa1', // organization ObjectId
     owner: '579334c75563625d6281b6f1', // adminUser ObjectId
     provider: '579334c75563625d6281b6f1', // adminUser ObjectId
     location: {
       coordinates: [100.56983534303, 13.730537951109],
     },
+    status: 'unverified',
+    is_archived: false,
   },
   {
     detail: casual.text,
-    organization: '57933111556362511181aaa1',
+    organization: '57933111556362511181aaa1', // organization ObjectId
     owner: '579334c75563625d6281b6f1', // adminUser ObjectId
     provider: '579334c75563625d6281b6f1', // adminUser ObjectId
     location: {
       coordinates: [100.56983534304, 13.730537951108],
     },
+    status: 'verified',
+    is_archived: false,
   },
   {
     detail: casual.text,
-    organization: '57933111556362511181aaa1',
+    organization: '57933111556362511181aaa1', // organization ObjectId
     owner: '579334c75563625d6281b6f1', // adminUser ObjectId
     provider: '579334c75563625d6281b6f1', // adminUser ObjectId
     location: {
       coordinates: [100.56983534305, 13.730537951107],
     },
+    status: 'assigned',
+    is_archived: false,
+    assigned_department: '57933111556362511181bbb1', // department ObjectId
   },
 ];
