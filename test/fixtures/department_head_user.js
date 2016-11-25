f4d2d97ab452b0a492aa8f444bdc3cd7a54905a0
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-25 18:41:22 +0700

    Prepare fixtures for users in each role

--- test/fixtures/department_head_user.js
+++ test/fixtures/department_head_user.js
@@ -1,18 +1,18 @@
-const SUPER_ADMIN = require('../../src/constants/roles').SUPER_ADMIN;
+const DEPARTMENT_HEAD = require('../../src/constants/roles').DEPARTMENT_HEAD;
 
 module.exports = {
-  _id: '579334c74443625d6281b6ff',
-  name: 'YouPin Super Admin',
+  _id: '579334c75553625d6281b6cc',
+  name: 'YouPin Department Head',
   phone: '081-985-2586',
   fb_id: 'youpin_fb_id',
   // hash of 'youpin_admin' password
   password: '$2a$10$iorOMFOPboPeF20W20DKruey2UXXa4eOQSuReOMlxXnqNe5t6Egaq',
-  email: 'super_admin@youpin.city',
+  email: 'department_head@youpin.city',
   organization_and_role_pairs: [
-    { organization: '579334c74443625d6281b6dd', role: SUPER_ADMIN },
+    { organization: '579334c74443625d6281b6dd', role: DEPARTMENT_HEAD },
   ],
   organization_and_department_pairs: [
     { organization: '579334c74443625d6281b6dd', department: '57933111556362511181bbb1' },
   ],
-  role: SUPER_ADMIN,
+  role: DEPARTMENT_HEAD,
 };
--- test/fixtures/departments.js
+++ test/fixtures/departments.js
@@ -1,8 +1,14 @@
 module.exports = [
   {
+    _id: '57933111556362511181ccc1',
+    name: 'Admin Department',
+    organization: '57933111556362511181aaa1',
+    detail: 'Admins live here',
+  },
+  {
     _id: '57933111556362511181bbb1',
     name: 'Department of Nerds',
-    organization: '57933111556362511181aaa1', // superAdminUser and adminUser
+    organization: '57933111556362511181aaa1',
     detail: 'An awesome department',
   },
 ];
