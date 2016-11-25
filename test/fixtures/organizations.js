67442862e048cc10024dcdc79cd3f3272b897736
Supasate Choochaisri <supasate.c@gmail.com>
2016-11-25 18:41:22 +0700

    Prepare fixtures for users in each role

--- test/fixtures/organizations.js
+++ test/fixtures/organizations.js
@@ -2,7 +2,8 @@ module.exports = [
   {
     _id: '57933111556362511181aaa1',
     name: 'YouPin',
-    users: ['579334c74443625d6281b6ff', '579334c75563625d6281b6f1'], // superAdminUser and adminUser
+    // superAdminUser, organizationAdminUser, departmentHeadUser
+    users: ['579334c74443625d6281b6ff', '579334c74443625d6281b6dd', '579334c75553625d6281b6cc'],
     detail: 'An awesome organization',
   },
 ];
