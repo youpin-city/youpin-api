daf451ccbe8b5250701f05fb247d829245aa14e5
parnurzeal <parnurzeal@gmail.com>
2016-11-27 11:44:52 +0800

    Make organization name unique

--- src/services/organization/organization-model.js
+++ src/services/organization/organization-model.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const organizationSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   detail: { type: String },
   created_time: { type: Date, default: Date.now },
