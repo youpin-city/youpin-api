6ad7c9ad0c0a8ee97db8ab9dddf8b19a40290b48
heytitle <pat.chormai@gmail.com>
2016-12-22 22:33:50 +0100

    Return assigned_user with user's information

--- src/services/pin/pin-model.js
+++ src/services/pin/pin-model.js
@@ -57,6 +57,11 @@ const pinSchema = new Schema({
 // Index geosearch field
 pinSchema.index({ location: '2dsphere' });
 
+pinSchema.pre('find', function(next, res) {
+    this.populate('assigned_user');
+    next();
+});
+
 const Pin = mongoose.model('Pin', pinSchema);
 
 module.exports = Pin;
