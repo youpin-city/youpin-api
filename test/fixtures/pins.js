5cde1899e59f671bae50375bcf227314d18d5d97
parnurzeal <parnurzeal@gmail.com>
2016-12-28 23:46:19 +0700

    Correct mock data to have correct assigned department

--- test/fixtures/pins.js
+++ test/fixtures/pins.js
@@ -4,8 +4,8 @@ const DEPARTMENT_GENERAL_ID = require('./constants').DEPARTMENT_GENERAL_ID;
 const ORGANIZATION_ID = require('./constants').ORGANIZATION_ID;
 const PIN_ASSIGNED_ID = require('./constants').PIN_ASSIGNED_ID;
 const PIN_ASSIGNED_DETAIL = require('./constants').PIN_ASSIGNED_DETAIL;
-const PIN_DEPARTMENT_UNASSIGNED_ID = require('./constants').PIN_DEPARTMENT_UNASSIGNED_ID;
-const PIN_DEPARTMENT_UNASSIGNED_DETAIL = require('./constants').PIN_DEPARTMENT_UNASSIGNED_DETAIL;
+const PIN_PROCESSING_ID = require('./constants').PIN_PROCESSING_ID;
+const PIN_PROCESSING_DETAIL = require('./constants').PIN_PROCESSING_DETAIL;
 const PIN_UNVERIFIED_ID = require('./constants').PIN_UNVERIFIED_ID;
 const PIN_UNVERIFIED_DETAIL = require('./constants').PIN_UNVERIFIED_DETAIL;
 const PIN_VERIFIED_ID = require('./constants').PIN_VERIFIED_ID;
@@ -14,7 +14,6 @@ const PIN_VERIFIED_DETAIL = require('./constants').PIN_VERIFIED_DETAIL;
 module.exports = [
   {
     _id: ObjectId(PIN_UNVERIFIED_ID), // eslint-disable-line new-cap
-    assigned_department: DEPARTMENT_GENERAL_ID, // department ObjectId
     created_time: '2016-12-01',
     detail: PIN_UNVERIFIED_DETAIL,
     organization: ORGANIZATION_ID, // organization ObjectId
@@ -28,7 +27,6 @@ module.exports = [
   },
   {
     _id: ObjectId(PIN_VERIFIED_ID), // eslint-disable-line new-cap
-    assigned_department: '57933111556362511181ccc1', // department ObjectId
     created_time: '2016-12-05',
     detail: PIN_VERIFIED_DETAIL,
     organization: ORGANIZATION_ID, // organization ObjectId
@@ -55,16 +53,17 @@ module.exports = [
     is_archived: false,
   },
   {
-    _id: ObjectId(PIN_DEPARTMENT_UNASSIGNED_ID), // eslint-disable-line new-cap
+    _id: ObjectId(PIN_PROCESSING_ID), // eslint-disable-line new-cap
+    assigned_department: '57933111556362511181ccc1', // department ObjectId
     created_time: '2016-12-03',
-    detail: PIN_DEPARTMENT_UNASSIGNED_DETAIL,
+    detail: PIN_PROCESSING_DETAIL,
     organization: ORGANIZATION_ID, // organization ObjectId
     owner: '579334c75563625d6281b6f1', // adminUser ObjectId
     provider: '579334c75563625d6281b6f1', // adminUser ObjectId
     location: {
       coordinates: [100.56983534305, 13.730537951107],
     },
-    status: 'unverified',
+    status: 'processing',
     is_archived: false,
   },
 ];
