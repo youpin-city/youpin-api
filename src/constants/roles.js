195bfbe9f38f3287bf6bfc6d01e2bfd54c8261f1
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-15 11:06:49 +0700

    Add public_relations role

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -2,6 +2,7 @@ module.exports = {
   DEPARTMENT_HEAD: 'department_head',
   DEPARTMENT_OFFICER: 'department_officer',
   ORGANIZATION_ADMIN: 'organization_admin',
+  PUBLIC_RELATIONS: 'public_relations',
   SUPER_ADMIN: 'super_admin',
   USER: 'user',
 };
