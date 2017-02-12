b2a0e693b6713157f793319f4c5c9b83edd75a7f
Supasate Choochaisri <supasate.c@gmail.com>
2017-02-12 14:50:41 +0800

    Make email to be not required

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -9,7 +9,6 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: {
     type: String,
-    required: true,
     unique: true,
     validate: { validator: validator.isEmail, message: '{VALUE} is not a valid email!' },
   },
