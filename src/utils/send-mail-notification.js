fa714d8e2bc746cd866864d4184c649a999b6e4a
parnurzeal <parnurzeal@gmail.com>
2017-04-16 10:13:55 +0800

    Add check when sending mail during testing (need to set TEST_MAIL_NOTIF=true)

--- src/utils/send-mail-notification.js
+++ src/utils/send-mail-notification.js
@@ -6,6 +6,10 @@ const sendMailNotification = (mailServiceConfig, email, message) => {
   if (!mailServiceConfig.providerConfig) {
     return Promise.reject('No proper mail service config. The notification will not be sent.');
   }
+  // Only send mail during test if a user forces it.
+  if (process.env.NODE_ENV === 'test' && process.env.TEST_MAIL_NOTIF !== 'true') {
+    return Promise.reject('To send mail during testing, please set TEST_MAIL_NOTIF=true.');
+  }
   const transporter = nodemailer.createTransport(mailServiceConfig.providerConfig);
   // TODO(A): Send to multiple emails at once and use a better html text format.
   const mailOptions = {
