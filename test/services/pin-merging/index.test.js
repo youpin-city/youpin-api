d17ec091f4d050bdaff0b54f7e0eaed03069aaeb
Supasate Choochaisri <supasate.c@gmail.com>
2017-04-28 23:24:36 -0700

    Allow all roles except normal user to merge pins

--- test/services/pin-merging/index.test.js
+++ test/services/pin-merging/index.test.js
@@ -1,22 +1,44 @@
 // Test helper functions
-const assertTestEnv = require('../../test_helper').assertTestEnv;
-const expect = require('../../test_helper').expect;
-const loadFixture = require('../../test_helper').loadFixture;
 const request = require('supertest-as-promised');
+const {
+  assertTestEnv,
+  expect,
+  loadFixture,
+  login,
+} = require('../../test_helper');
 
 // Models
 const Department = require('../../../src/services/department/department-model');
 const Pin = require('../../../src/services/pin/pin-model');
 const User = require('../../../src/services/user/user-model');
 
+// Roles
+const {
+  DEPARTMENT_HEAD,
+  DEPARTMENT_OFFICER,
+  ORGANIZATION_ADMIN,
+  PUBLIC_RELATIONS,
+  SUPER_ADMIN,
+} = require('../../../src/constants/roles');
+
 // Fixture
 const departmentHeadUser = require('../../fixtures/department_head_user');
-const departments = require('../../fixtures/departments');
+const departmentOfficerUser = require('../../fixtures/department_officer_user');
+const normalUser = require('../../fixtures/normal_user');
 const organizationAdminUser = require('../../fixtures/organization_admin_user');
-const pins = require('../../fixtures/pins');
+const publicRelationsUser = require('../../fixtures/public_relations_user');
 const superAdminUser = require('../../fixtures/super_admin_user');
-const PIN_ASSIGNED_ID = require('../../fixtures/constants').PIN_ASSIGNED_ID;
-const PIN_PENDING_ID = require('../../fixtures/constants').PIN_PENDING_ID;
+const departments = require('../../fixtures/departments');
+const pins = require('../../fixtures/pins');
+const {
+  PIN_ASSIGNED_ID,
+  PIN_PENDING_ID,
+  USER_DEPARTMENT_HEAD_ID,
+  USER_DEPARTMENT_OFFICER_ID,
+  USER_ORGANIZATION_ADMIN_ID,
+  USER_PUBLIC_RELATIONS_ID,
+  USER_SUPER_ADMIN_ID,
+} = require('../../fixtures/constants');
 
 // App stuff
 const app = require('../../../src/app');
@@ -34,10 +56,12 @@ describe('Pin merging service', () => {
       // Create admin user and app3rd for admin
       Promise.all([
         loadFixture(Department, departments),
-        loadFixture(User, superAdminUser),
-        loadFixture(User, organizationAdminUser),
         loadFixture(User, departmentHeadUser),
-        loadFixture(Pin, pins),
+        loadFixture(User, departmentOfficerUser),
+        loadFixture(User, normalUser),
+        loadFixture(User, organizationAdminUser),
+        loadFixture(User, publicRelationsUser),
+        loadFixture(User, superAdminUser),
       ])
       .then(() => {
         done();
@@ -46,6 +70,16 @@ describe('Pin merging service', () => {
         done(err);
       });
     });
+  });
+
+  beforeEach((done) => {
+    loadFixture(Pin, pins)
+      .then(() => done());
+  });
+
+  afterEach((done) => {
+    Pin.remove({})
+      .then(() => done());
   });
 
   after((done) => {
@@ -80,13 +114,13 @@ describe('Pin merging service', () => {
       const token = loginResp.body.token;
 
       return request(app)
-      .post(`/pins/${PIN_PENDING_ID}/merging`)
-      .set('Authorization', `Bearer ${token}`)
-      .set('Content-type', 'application/json')
-      .send({
-        mergedParentPin: PIN_ASSIGNED_ID,
-      })
-      .expect(201);
+        .post(`/pins/${PIN_PENDING_ID}/merging`)
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-type', 'application/json')
+        .send({
+          mergedParentPin: PIN_ASSIGNED_ID,
+        })
+        .expect(201);
     })
     .then((mergingResp) => {
       expect(mergingResp.body.length).to.equal(2);
@@ -110,5 +144,117 @@ describe('Pin merging service', () => {
       done();
     })
     .catch(err => done(err));
+  });
+
+  describe('allows all roles except normal user to merge pin', () => {
+    const allowedUpdatingUsers = [
+      {
+        role: DEPARTMENT_HEAD,
+        id: USER_DEPARTMENT_HEAD_ID,
+        email: 'department_head@youpin.city',
+        password: 'youpin_admin',
+      },
+      {
+        role: DEPARTMENT_OFFICER,
+        id: USER_DEPARTMENT_OFFICER_ID,
+        email: 'department_officer@youpin.city',
+        password: 'youpin_department_officer',
+      },
+      {
+        role: ORGANIZATION_ADMIN,
+        id: USER_ORGANIZATION_ADMIN_ID,
+        email: 'organization_admin@youpin.city',
+        password: 'youpin_admin',
+      },
+      {
+        role: PUBLIC_RELATIONS,
+        id: USER_PUBLIC_RELATIONS_ID,
+        email: 'public_relations@youpin.city',
+        password: 'youpin_public_relations',
+      },
+      {
+        role: SUPER_ADMIN,
+        id: USER_SUPER_ADMIN_ID,
+        email: 'super_admin@youpin.city',
+        password: 'youpin_admin',
+      },
+    ];
+
+    allowedUpdatingUsers.forEach((user) => {
+      it(`allows ${user.role} to merge pin`, (done) => {
+        login(app, user.email, user.password)
+          .then((tokenResp) => {
+            const token = tokenResp.body.token;
+
+            if (!token) {
+              return done(new Error('No token returns'));
+            }
+
+            return request(app)
+              .post(`/pins/${PIN_PENDING_ID}/merging`)
+              .set('Authorization', `Bearer ${token}`)
+              .set('Content-type', 'application/json')
+              .send({
+                mergedParentPin: PIN_ASSIGNED_ID,
+              })
+              .expect(201);
+          })
+          .then((mergingResp) => {
+            expect(mergingResp.body.length).to.equal(2);
+            const updatedChildPinResult = mergingResp.body[0];
+            const updatedParentPinResult = mergingResp.body[1];
+
+            expect(updatedChildPinResult.ok).to.equal(1);
+            expect(updatedParentPinResult.ok).to.equal(1);
+
+            return Pin.findOne({ _id: PIN_PENDING_ID }); // eslint-disable-line no-underscore-dangle
+          })
+          .then(updatedChildPin => {
+            expect(updatedChildPin.is_merged).to.equal(true);
+            expect(updatedChildPin.merged_parent_pin).to.deep.equal(ObjectId(PIN_ASSIGNED_ID)); // eslint-disable-line new-cap,max-len
+
+            return Pin.findOne({ _id: PIN_ASSIGNED_ID }).lean();
+          })
+          .then(updatedParentPin => {
+            expect(updatedParentPin.merged_children_pins).to.deep.equal([ObjectId(PIN_PENDING_ID)]); // eslint-disable-line new-cap,max-len
+
+            done();
+          })
+          .catch(err => done(err));
+      });
+    });
+
+    it('returns 401 not allowing normal user to update pin', (done) => (
+      request(app)
+        .post('/auth/local')
+        .send({
+          email: 'user@youpin.city',
+          password: 'youpin_user',
+        })
+        .then((tokenResp) => {
+          const token = tokenResp.body.token;
+
+          if (!token) {
+            return done(new Error('No token returns'));
+          }
+          return request(app)
+            .post(`/pins/${PIN_PENDING_ID}/merging`)
+            .set('Authorization', `Bearer ${token}`)
+            .set('Content-type', 'application/json')
+            .send({
+              mergedParentPin: PIN_ASSIGNED_ID,
+            });
+        })
+        .then((res) => {
+          const error = res.body;
+
+          expect(error.code).to.equal(403);
+          expect(error.name).to.equal('Forbidden');
+          expect(error.message).to.equal(
+            'You do not have valid permissions to access this.');
+          done();
+        })
+        .catch(error => console.log(error))
+    ));
   });
 });
