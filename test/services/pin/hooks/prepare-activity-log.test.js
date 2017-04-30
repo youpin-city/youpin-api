96bdbb01efabdcde19388fd6265379a52c015886
parnurzeal <parnurzeal@gmail.com>
2017-04-30 18:44:12 +0800

    Better email template format of changed data

--- test/services/pin/hooks/prepare-activity-log.test.js
+++ test/services/pin/hooks/prepare-activity-log.test.js
@@ -78,12 +78,7 @@ describe('Prepare Activity Log Hook', () => {
       // progress detail
       const newProgressDetail = mockHook.data.$push.progresses.detail;
       // description
-      const expectedDescription =
-        `${adminUser.name} edited following field:\n` +
-        ` - Edit [detail] from "${previousDetail}" to "${newDetail}"\n` +
-        ` - Edit [location] from "${previousLocation}" to "${newLocation}"\n` +
-        `${adminUser.name} added more data to following field:\n` +
-        ` - Add [progresses] with "${newProgressDetail}"`;
+      const expectedDescription = `${adminUser.name} edited pin #${pinId}`;
       const expectedLogInfo = {
         user: adminUser.name,
         organization: mongoose.Types.ObjectId(pins[0].organization), // eslint-disable-line new-cap,max-len
